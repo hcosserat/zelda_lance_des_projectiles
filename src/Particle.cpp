c554fbd3aae07cc56f34a1ea71b5373326cc8518
hippolytecosserat <hippolytecosserat@gmail.com>
2025-09-10 15:30:07 -0400

    Feat : particles

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,1 +1,9 @@
 #include "Particle.h"
+
+Particle::Particle(const Vector pos, const Vector vel, const Vector acc, const float mass) {
+    this->pos = pos;
+    this->vel = vel;
+    this->acc = acc;
+    this->mass = mass;
+    this->inverseMass = 1 / mass;
+}
--- src/Particle.h
+++ src/Particle.h
@@ -6,6 +6,12 @@ class Particle {
 public:
     Vector pos, vel, acc;
     float mass;
+    float inverseMass;
 
-
+    // Constructor
+    explicit Particle(
+        Vector pos = Vector{0, 0, 0},
+        Vector vel = Vector{0, 0, 0},
+        Vector acc = Vector{0, 0, 0},
+        float mass = 1);
 };
