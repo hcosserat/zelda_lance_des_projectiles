9c9e42af7905ed6eb392a935d74d97dcce1f54ae
Emeline <emeline.gehanno@gmail.com>
2025-10-01 15:03:35 -0400

    Add : ParticleForceRegistry

---
+++ src/ParticleForceRegistry.cpp
@@ -0,0 +1,28 @@
+#include "ParticleForceRegistry.h"
+
+void ParticleForceRegistry::add(Particle* particle, ParticleForceGenerator* fg)
+{
+	// TODO : Complete this method
+	/*
+	ParticleForceRegistration registration;
+	registration.particle = particle;
+	registration.fg = fg;
+	registrations.push_back(registration);
+	*/
+}
+
+void ParticleForceRegistry::remove(Particle* particle, ParticleForceGenerator* fg)
+{
+	// TODO : Complete this method
+}
+
+void ParticleForceRegistry::updateForces(float duration)
+{
+	// TODO : Complete this method
+	/*
+	for (auto& x : registrations) {
+		x.fg->updateforce(*x.particle, duration);
+	}
+	*/
+	
+}
---
+++ src/ParticleForceRegistry.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <Particle.h>
+#include <ParticleForceGenerator.h>
+class ParticleForceRegistry
+{
+
+	// Keeps track of all particles and their corresponding force generators
+	struct ParticleForceRegistration
+	{
+		Particle* particle;
+		ParticleForceGenerator* fg;
+	};
+
+	// Dynamic array of registrations
+	typedef std::vector<ParticleForceRegistration> Registry;
+
+	// Add a new registration to the registry
+	void add(Particle* particle, ParticleForceGenerator* fg);
+	// Remove a registration from the registry
+	void remove(Particle* particle, ParticleForceGenerator* fg);
+	// Delete the registry but not the particles or force generators
+	void clear();
+	void updateForces(float duration);
+
+};
+
