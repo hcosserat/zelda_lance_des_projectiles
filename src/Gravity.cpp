e8d6d666355c9f447e742e4bbcb65d56c57fa00d
d23jasic <dimitri.jasica@imt-atlantique.net>
2025-10-01 21:38:26 +0200

    Add : Ajout de la classe Gravity pour la force de gravité

---
+++ src/Gravity.cpp
@@ -0,0 +1,8 @@
+#include "Gravity.h"
+#include "src/Particle.h"
+
+void ParticleGravity::updateForce(Particle * p, float /*dt*/) {
+	if (!p) return;
+	if (p->inverseMass == 0.0f) return; // masse infinie
+	p->acc += gravity; // a += g   (F/m = g)
+}
---
+++ src/Gravity.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "src/ParticleForceGenerator.h"
+#include "src/Vector.h"
+
+class Particle;
+
+// Générateur de force de gravité
+class ParticleGravity : public ParticleForceGenerator {
+
+public:
+
+	Vector gravity;
+
+	// Initialise la gravité
+	explicit ParticleGravity(const Vector & g) : gravity(g) { }
+
+	// Modifie la gravité
+	void set(const Vector & g) { gravity = g; }
+	const Vector & get() const { return gravity; }
+
+	// Applique la force de gravité sur la particule
+	virtual void updateForce(Particle * particle, float duration);
+};
