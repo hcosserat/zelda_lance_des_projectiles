236c2f38bbedee4dd8e3ae5dc0cf58d9af6f79e0
Florian Heme <florian.heme@laposte.net>
2025-10-21 16:39:59 -0400

    ADD : Friction force

--- src/Forces/FrictionForce.h
+++ src/Forces/FrictionForce.h
@@ -7,18 +7,16 @@ class Particle;
 
 class FrictionForce : public ParticleForceGenerator {
 public:
-	float coef_friction;
+	float coef_friction_sta;
+	float coef_friction_cin;
 	Vector vec_norm;
 
 	// Initialise la friction
 	explicit FrictionForce(
-		float coef = 1.0,
+		float coef_sta = 10.0,
+		float coef_cin = 1.0,
 		Vector n = Vector { 0, 1, 0 }
 	);
-
-	// Modifie la friction
-	void set(const float & coef) { coef_friction = coef; }
-	const float & get() const { return coef_friction; }
 
 	// Applique la force de friction sur la particule
 	virtual void updateforce(Particle * particle, float duration);
