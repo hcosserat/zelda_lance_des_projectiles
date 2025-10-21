9169663fc552ff90390f236adf870e12b680fdb5
hippolytecosserat <hippolytecosserat@gmail.com>
2025-10-21 14:51:04 -0400

    Fix : suppression de Blob.center pour éviter la duplicité d'information

--- src/Actors/Blob.h
+++ src/Actors/Blob.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <Circle.h>
+#include "Circle.h"
 #include "../Forces/SpringForce.h"
 #include "../Forces/ParticleForceRegistry.h"
 
@@ -11,7 +11,7 @@ public:
 
     explicit Blob(
         const Particle &centerParticle = Particle(Vector(400, 500, 0), Vector(0, 0, 0),
-                                                   Vector(0, 0, 0), 100, Vector(0, 0, 0)),
+                                                  Vector(0, 0, 0), 100, Vector(0, 0, 0)),
         const float centerRadius = 18,
         const std::vector<Circle> &circles = {})
         : Actor(centerParticle), centerRadius(centerRadius), circles(circles) {
