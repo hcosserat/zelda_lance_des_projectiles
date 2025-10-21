6abc6b07260280a8637fd9be64e95bbe7dc6d1fa
Emeline <emeline.gehanno@gmail.com>
2025-10-21 16:44:34 -0400

    FEAT: Ajout du câble si limite élasticité dépassée

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -7,9 +7,9 @@ void ofApp::setup() {
     world = World();
 
     // Get the blob
-    for (Actor *actor: world.actors) {
+    for (Actor* actor : world.actors) {
         if (actor->getShape() == BlobShape) {
-            blob = dynamic_cast<Blob *>(actor);
+            blob = dynamic_cast<Blob*>(actor);
             break;
         }
     }
@@ -54,48 +54,48 @@ void ofApp::exit() {
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key) {
     switch (key) {
-        case 'a':
-            blob->addCircle();
-            break;
-        case 'r':
-            blob->removeCircle();
-            break;
-        case 's':
-            blob->separateCircle();
-            break;
-        case 'f':
-            blob->fusionCircle();
-            break;
+    case 'a':
+        blob->addCircle();
+        break;
+    case 'r':
+        blob->removeCircle();
+        break;
+    case 's':
+        blob->separateCircle();
+        break;
+    case 'f':
+        blob->fusionCircle();
+        break;
         // Left arrow to move left
-        case OF_KEY_LEFT:
-            blob->centerParticle.vel.x = -100;
-            break;
+    case OF_KEY_LEFT:
+        blob->centerParticle.vel.x = -100;
+        break;
         // Right arrow to move right
-        case OF_KEY_RIGHT:
-            blob->centerParticle.vel.x = 100;
-            break;
+    case OF_KEY_RIGHT:
+        blob->centerParticle.vel.x = 100;
+        break;
         // Space to jump
-        case ' ':
-            blob->centerParticle.vel.y = -250;
-            break;
-        default:
-            break;
+    case ' ':
+        blob->centerParticle.vel.y = -250;
+        break;
+    default:
+        break;
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key) {
     switch (key) {
-        case OF_KEY_LEFT:
-        case OF_KEY_RIGHT:
-            blob->centerParticle.vel.x = 0;
-            break;
-        case ' ':
-            blob->centerParticle.vel.y = 0;
-            // TODO : No need to set to 0, just let gravity do its job but doesn't work well for now
-            break;
-        default:
-            break;
+    case OF_KEY_LEFT:
+    case OF_KEY_RIGHT:
+        blob->centerParticle.vel.x = 0;
+        break;
+    case ' ':
+        blob->centerParticle.vel.y = 0;
+        // TODO : No need to set to 0, just let gravity do its job but doesn't work well for now
+        break;
+    default:
+        break;
     }
 }
 
